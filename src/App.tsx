a5bdc285134aa2da4f42ab6edad045521b9822c5
Lucas Novaes <lucasmsn200@gmail.com>
2020-01-15 22:29:33 -0300

    finalizando mini projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "../src/components/layout/navbar";
+import MyKart from "../src/components/pages/mykart";
+import MyProducts from "../src/components/pages/myproducts";
+import { MainPage } from "../src/components/pages/main";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../src/core/redux/store";
 
-const App: React.FC = () => {
-
+function App() {
   return (
-    <Provider store={store}>
-    <div className="App">
-      <h1> Hello World! </h1>
-    </div>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route path="/meus-produtos">
+                  <MyProducts />
+                </Route>
+                <Route path="/meu-carrinho">
+                  <MyKart />
+                </Route>
+                <Route path="/">
+                  <MainPage />
+                </Route>
+              </Switch>
+            </div>
+            <ToastContainer
+              autoClose={5000}
+              position={toast.POSITION.BOTTOM_RIGHT}
+            />
+          </div>
+        </Router>
+      </Provider>
+    </>
   );
 }
 
